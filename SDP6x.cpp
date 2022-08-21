9dd9b8a863cb0a9ea305b187cc3e76f6a4710b4c
Damien L <damienleb@protonmail.com>
2022-08-21 21:10:21 +0200

    added first take of sdp7xx

---
+++ SDP6x.cpp
@@ -0,0 +1,165 @@
+/*
+  SDP6x - A Low Pressor Sensor Library for Arduino.
+
+  Supported Sensor modules:
+    SDP600 series from Sensirion - http://www.futureelectronics.com/en/Technologies/Product.aspx?ProductID=SDP610125PASENSIRIONAG1050689&IM=0
+  
+  Created by Christopher Ladden at Modern Device on December 2009.
+  Modified by Paul Badger March 2010
+  Modified by www.misenso.com on October 2011:
+  - code optimisation
+  - compatibility with Arduino 1.0
+  SDP6x Modified above by Antony Burness Jun 2016.
+  - adapted for the I2C Pressure sensor
+  - using example from sensirion https://www.sensirion.com/products/differential-pressure-sensors/all-documents-of-sensirions-differential-pressure-sensors-for-download/
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/ 
+
+#include "SDP6x.h"
+#include <inttypes.h>
+#include "mbed.h"
+
+#define SCALEFACTOR 60.0
+// NOTE you will need to change the SCALEFACTOR to the appropriate value for your sensor
+//  Don't forget the .0 at the end, it makes sure Arduino does not round the number
+// SENSOR       | SDP6xx-500Pa and SDP5xx  |  SDP6x0-125Pa  |  SDP6x0-25Pa  |
+// SCALEFACTOR  |         60.0             |     240.0      |     1200.0    | (1/Pa)
+
+/******************************************************************************
+ * Global Functions
+ ******************************************************************************/
+
+SDP6x::SDP6x(I2C &i2c_, char i2c_address)
+    : i2c(i2c_), address(i2c_address)
+{
+    i2c.frequency(400000);
+}
+
+SDP6x::~SDP6x()
+{
+}
+
+bool SDP6x::init()
+{
+    i2c.lock();
+    i2c.start();
+    if (!i2c.write(0x80, NULL, 0)){ // 0 is OK
+        return true;
+    } else {
+        return false;
+    }
+    i2c.stop();
+    i2c.unlock();
+}
+/**********************************************************
+ * GetPressureDiff
+ *  Gets the current Pressure Differential from the sensor.
+ *
+ * @return float - The Pressure in Pascal
+ **********************************************************/
+float SDP6x::GetPressureDiff(void)
+{
+  int16_t res;
+  if (readSensor(ePresHoldCmd, (uint16_t*)&res)) {
+    return ((float)(res)/SCALEFACTOR);
+  } else {
+      //return ((float)(res)/SCALEFACTOR);
+      return -10000000;
+  }
+}
+
+/******************************************************************************
+ * Private Functions
+ ******************************************************************************/
+
+bool SDP6x::readSensor(char command, uint16_t* res)
+{
+    uint16_t result;
+    char data[3];
+
+    i2c.lock();
+    i2c.start();
+    i2c.write(0x80, &command, 1);
+    i2c.stop();
+    i2c.unlock();
+    
+    // Let's read it back
+    // Waiting ???
+    ThisThread::sleep_for(50ms);
+    // Go !
+    i2c.read(address << 1, data, 3);
+
+    if (CheckCrc(data, 2, data[2]) == NO_ERROR) {
+        result = (data[0] << 8);
+        result += data[1];
+        *res = result; 
+        return true;
+    }
+    return false;
+}
+
+void SDP6x::SetSensorResolution(etSensorResolutions resolution){
+    uint16_t    userRegister; // advanced user register
+    
+    if (readSensor(eReadUserReg, &userRegister)) {
+        userRegister &= 0xF1FF;
+        userRegister |= (resolution & 0x07) << 9;
+        writeSensor(userRegister);
+    }
+}
+
+void SDP6x::writeSensor(uint16_t data){
+    char bytes[2];
+
+    bytes[0] = (data & 0xFF00) >> 8;
+    bytes[1] = data & 0x00FF;
+    
+    i2c.lock();
+    i2c.start();
+    i2c.write(0x80);
+    i2c.write(eWriteUserReg); // send the pointer location
+    for(uint16_t i = 0; i < 2; i++)
+        i2c.write(bytes[i]); //send the data
+    i2c.stop();
+    i2c.unlock();
+}
+
+//============================================================
+// From sensirion App Note "CRC Checksum"
+//calculates checksum for n bytes of data
+//and compares it with expected checksum
+//input: data[] checksum is built based on this data
+// nbrOfBytes checksum is built for n bytes of data
+// checksum expected checksum
+//return: error: CHECKSUM_ERROR = checksum does not match
+// 0 = checksum matches
+//============================================================ 
+PRES_SENSOR_ERROR SDP6x::CheckCrc(char data[], char nbrOfBytes, char checksum) {
+    char crc = 0;
+    char byteCtr;
+    //calculates 8-Bit checksum with given polynomial
+    for (byteCtr = 0; byteCtr < nbrOfBytes; ++byteCtr) { 
+        crc ^= (data[byteCtr]);
+        for (char bit = 8; bit > 0; --bit) { 
+            if (crc & 0x80) crc = (crc << 1) ^ POLYNOMIAL;
+            else crc = (crc << 1);
+        }
+    }
+    if (crc != checksum) return CHECKSUM_ERROR;
+    else return NO_ERROR;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -9,18 +9,24 @@
 //#include "DSHOT150.h"
 #include <cstdint>
 #include "Adafruit_SSD1306.h" 
+#include "SDP6x.h"
 
-#define PC_DEBUG_ON         0
-#define SSD_I2C_ADDRESS     0x78
-#define SSD1306_ON          1
-#define MAX_BUFFER          129
+#define AUTO_MODE                         0
+#define PC_DEBUG_ON                       0
+#define SSD_I2C_ADDRESS                   0x78
+#define SSD1306_ON                        1
+#define MAX_BUFFER                        129
 // Need to move into a config pot
-#define THROTTLE_MIN        0.1f
-#define ESC_THROTTLE_MAX    0.6f
-#define THROTTLE_START      0.5f
-#define POT_REFRESH_TIME    20ms
-#define NPA_REFRESH_TIME    200ms
-
+#define THROTTLE_MIN                      0.15f
+#define ESC_THROTTLE_MAX                  0.6f
+#define THROTTLE_START                    0.5f
+#define SCREEN_REFRESH_TIME               100ms
+#define POT_REFRESH_TIME                  10ms
+#define POT_MODE_REFRESH_STEPS            0.04f
+#define NPA_REFRESH_TIME                  20ms
+#define SDP_REFRESH_TIME                  50ms
+#define AUTO_MODE_REFRESH_TIME            5ms
+#define AUTO_MODE_REFRESH_STEPS           0.01f
 class I2CPreInit : public I2C
 {
 public:
@@ -30,28 +36,41 @@ public:
         start();
     };
 };
-I2CPreInit gI2C(PB_7, PB_6); // SDA , SCL
-Adafruit_SSD1306_I2c display(gI2C, PA_0);// Mystere
+I2CPreInit                                gI2C(PB_4, PA_7); // SDA , SCL
+Adafruit_SSD1306_I2c                      display(gI2C, PB_1);// Mystere
+ESC                                       soufflette(PA_3);
+I2C                                       sdpI2C(PB_7, PB_6);  
+SDP6x                                     sdp(sdpI2C);
 
-AnalogIn  pot(PA_4);
-AnalogIn  npa(PA_5);
-AnalogIn  adc_temp(ADC_TEMP);
-AnalogIn  adc_vref(ADC_VREF);
-//DSHOT150       soufflette(PE_9);
-DigitalOut led(PB_3);
-ESC       soufflette(PA_3);
+AnalogIn                                  pot(PA_4);
+AnalogIn                                  npa(PA_0);
+AnalogIn                                  adc_temp(ADC_TEMP);
+AnalogIn                                  adc_vref(ADC_VREF);
+//DSHOT150                                  soufflette(PE_9);
+DigitalOut                                led(PB_3);
 
 char buffer[MAX_BUFFER];
 
+Ticker ScreenRefreshReady;
 Ticker PotsampleReady;
 Ticker NpasampleReady;
+Ticker SdpsampleReady;
+Ticker AutoModeReady;
+bool screen_refresh_timer_ready;
 bool pot_sample_timer_ready;
 bool npa_sample_timer_ready;
+bool sdp_sample_timer_ready;
+bool auto_mode_ready;
+float auto_mode;
 
 void update_info_screen(char* info);
-void update_esc_screen(float esc_speed, int npa_value);
+void update_esc_screen(float esc_speed, int npa_value, float sdp_value);
 void update_esc(float speed);
-void update_pc_debug(float esc_speed, int npa_value);
+void update_pc_debug(float esc_speed, int npa_value, float sdp_value);
+
+void screen_refresh_timer() {
+    screen_refresh_timer_ready = true;
+}
 
 void pot_sample_timer() {
     pot_sample_timer_ready = true;
@@ -61,7 +80,18 @@ void npa_sample_timer() {
     npa_sample_timer_ready = true;
 }
 
+void sdp_sample_timer() {
+    sdp_sample_timer_ready = true;
+}
+
+void auto_mode_timer() {
+    auto_mode_ready = true;
+}
+
 int main() {
+    auto_mode = THROTTLE_START;
+    memset(buffer, 0, sizeof(buffer));
+
     display.clearDisplay();
     display.setTextCursor(0,0);
     display.printf ("-- SOUFFLETTE --");
@@ -70,36 +100,66 @@ int main() {
     update_info_screen("wait for init...");
 
     float pot_value = 0.0f;
+    float sdp_value = 0.0f;
     unsigned short npa_value = 0;
 
-    update_esc(pot_value);
-    ThisThread::sleep_for(4000ms);
+    update_esc(0.0f);
+
+    ThisThread::sleep_for(2000ms);
+    if (sdp.init()){
+        update_info_screen("sdp7xx on...");
+    }
+    ThisThread::sleep_for(2000ms);
 
     update_info_screen("set to min speed");
     update_esc(THROTTLE_START);
     ThisThread::sleep_for(2000ms);
     update_info_screen("initialisaton ok");
 
+    ScreenRefreshReady.attach(&screen_refresh_timer, SCREEN_REFRESH_TIME);
     PotsampleReady.attach(&pot_sample_timer, POT_REFRESH_TIME);
     NpasampleReady.attach(&npa_sample_timer, NPA_REFRESH_TIME);
+    SdpsampleReady.attach(&sdp_sample_timer, SDP_REFRESH_TIME);
+    AutoModeReady.attach(&auto_mode_timer, AUTO_MODE_REFRESH_TIME);
 
     // ENTERING LOOP, BUT CLEAN BEFORE :)
     display.clearDisplay();
 
     while (1) {
+        if (screen_refresh_timer_ready){
+            led = !led;
+            update_esc_screen(pot_value, npa_value, sdp_value);
+            screen_refresh_timer_ready = false;
+        }
         if (pot_sample_timer_ready){
             pot_value = pot.read()*(1.0f - THROTTLE_MIN) + THROTTLE_MIN;
             if (pot_value > 1.0f) pot_value = 1.0f;
-            update_esc(pot_value);
-            update_esc_screen(pot_value, npa_value);
+            if (!AUTO_MODE) update_esc(pot_value);
             pot_sample_timer_ready = false;
         }
         if (npa_sample_timer_ready){
-            led = !led;
             npa_value = npa.read_u16();
             npa_sample_timer_ready = false;
             if(PC_DEBUG_ON == 1)
-            update_pc_debug(pot_value, npa_value);
+            update_pc_debug(pot_value, npa_value, sdp_value);
+        }
+        if (sdp_sample_timer_ready){
+            sdp_value = sdp.GetPressureDiff();
+            sdp_sample_timer_ready = false;
+            if(PC_DEBUG_ON == 1)
+            update_pc_debug(pot_value, npa_value, sdp_value);
+        }
+        if (AUTO_MODE && auto_mode_ready){
+            if (npa_value > pot.read_u16()){
+                auto_mode = auto_mode - AUTO_MODE_REFRESH_STEPS ;
+                if (auto_mode <= THROTTLE_MIN) auto_mode = THROTTLE_MIN;
+                update_esc(auto_mode);
+            } else {
+                auto_mode = auto_mode + AUTO_MODE_REFRESH_STEPS;
+                if (auto_mode > 1.0f) auto_mode = 1.0f;
+                update_esc(auto_mode);
+            }
+            auto_mode_ready = false;
         }
     }
     return 0;
@@ -120,14 +180,18 @@ void update_esc(float speed){
     soufflette.pulse();
 }
 
-void update_esc_screen(float esc_speed, int npa_value){     
+void update_esc_screen(float esc_speed, int npa_value, float sdp_value){     
     // ESC_SPEED REFRESH
     display.clearDisplay();
     display.setTextCursor(20,0);
     display.printf ("-- SOUFFLETTE --");
-    display.setTextCursor(0,25);
+    display.setTextCursor(0,15);
     sprintf(buffer, "TEMP            ");
     sprintf(buffer + strlen(buffer), "%05f", (adc_temp.read()*100));
+    display.printf(buffer);
+    display.setTextCursor(0,35);
+    sprintf(buffer, "DIFF PRESS      ");
+    sprintf(buffer + strlen(buffer), "%f", sdp_value);
     display.printf(buffer);
     display.setTextCursor(0,35);
     sprintf(buffer, "PRESSURE        ");
@@ -154,9 +218,11 @@ void update_esc_screen(float esc_speed, int npa_value){
     display.display();
 }
 
-void update_pc_debug(float esc_speed, int npa_value){
+void update_pc_debug(float esc_speed, int npa_value, float sdp_value){
     sprintf(buffer, "TEMP = ");
     sprintf(buffer + strlen(buffer), "%05f ", (adc_temp.read()*100));
+    sprintf(buffer + strlen(buffer), "| DIFF PRESS = ");
+    sprintf(buffer + strlen(buffer), "%f ", sdp_value);
     sprintf(buffer + strlen(buffer), "| PRESSURE = ");
     sprintf(buffer + strlen(buffer), "%05d ", npa_value);
     sprintf(buffer + strlen(buffer), "| SPEED = ");
